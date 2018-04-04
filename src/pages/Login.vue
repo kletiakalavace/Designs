3b496fd861bc02c387152145ad5363cb778a4863
kletiakalavace <kletia@manoolia.com>
2018-04-04 16:03:38 +0200

    fixes

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,7 +2,7 @@
     <v-container fluid fill-height class="login-container">
         <div class="login-background"></div>
 
-        <v-layout align-center justify-center row wrap>
+        <v-layout align-center justify-center row wrap class="login-layout">
             <v-flex xs12 sm6 md4 class="login-main">
                 <v-card class="elevation-6 form-content">
                     <v-form lazy-validation>
@@ -76,20 +76,21 @@
                     </v-snackbar>
                 </v-card>
             </v-flex>
-            <div class="xs12 footer-loginPage">
-                    <v-list class="text-md-center login-list">
-                        <v-list-tile
-                                class="d-inline-block"
-                                v-for="item in menuItems"
-                                :key="item.title"
-                                router
-                                :to="item.link">
-                            <v-list-tile-content class="login-navigation-bottom">
-                                {{item.title}}
-                            </v-list-tile-content>
-                        </v-list-tile>
-                    </v-list>
-            </div>
+
+            <v-flex xs12 class="footer-loginPage">
+                <v-list class="text-md-center login-list">
+                    <v-list-tile
+                            class="d-inline-block"
+                            v-for="item in menuItems"
+                            :key="item.title"
+                            router
+                            :to="item.link">
+                        <v-list-tile-content class="login-navigation-bottom">
+                            {{item.title}}
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+            </v-flex>
         </v-layout>
 
     </v-container>
@@ -155,169 +156,156 @@
 </script>
 
 <style lang="scss">
-    @import '~@/assets/sass/variables.scss';
-    html{
-        overflow: auto !important;
+  @import '~@/assets/sass/variables.scss';
+
+  html {
+    overflow: auto !important;
+  }
+
+  .login-container {
+    .login-layout {
+      height: auto !important;
+      flex-direction: column;
+      width:100%
     }
-    .footer-loginPage{
-        display: -webkit-box;
-            display: -webkit-flex;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-pack: justify;
-            -webkit-justify-content: space-between;
-            -ms-flex-pack: justify;
-            justify-content: space-between;
-            -webkit-align-self: flex-end;
-            -ms-flex-item-align: end;
-            align-self: flex-end;
-            -webkit-align-items: center;
-            -webkit-box-align: center;
-            -ms-flex-align: center;
-            align-items: center;
+
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    min-height: 100vh;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    background: #f5f6f8;
+
+    .footer-loginPage {
+      margin-top: 20px;
+
     }
-    .login-container {
-        display: -webkit-box;
-        display: -webkit-flex;
-        display: -ms-flexbox;
-        display: flex;
-        min-height: 100vh;
-        -webkit-flex-direction: column;
-        -ms-flex-direction: column;
-        flex-direction: column;
-         background: #f5f6f8;
-        .alert.error-message{
-          color:$error-message;
-          font-size: 12px;
-          font-family: 'Open Sans', sans-serif;
-          background:transparent
-          ;
+    .alert.error-message {
+      color: $error-message;
+      font-size: 12px;
+      font-family: 'Open Sans', sans-serif;
+      background: transparent;
+    }
+    .notification-link {
+      display: block;
+      color: #1754be !important;
+      text-transform: uppercase;
+      font-size: 13px;
+      font-weight: 600;
+      float: left;
+      text-decoration: none;
+      font-family: 'Open Sans', sans-serif;
+      margin-top: 10px;
+    }
+    .close-notification {
+      color: $grey--text;
+      top: 9px;
+      position: absolute;
+      right: 9px;
+      box-shadow: none !important;
+      background: $white;
+      .icon {
+        font-size: 19px;
+        font-weight: 600;
+      }
+    }
+    .login-background {
+      position: absolute;
+      min-height: 50%;
+      width: 100%;
+      background-image: url(../assets/img/login-background.jpg);
+      background-size: cover;
+      top: 0;
+      left: 0;
+      z-index: 1;
+    }
+
+    .login-logo {
+      margin: auto;
+      display: block;
+      width: 111px;
+    }
+    .login-main {
+      min-width: 410px;
+      z-index: 99;
+      flex: 1;
+      justify-content: center;
+      align-items: center;
+      display: flex;
+      margin: 0;
+      padding: 0;
+    }
+    .form-content {
+      padding: 25px;
+      width:100%;
+      label {
+        color: $black;
+        opacity: 0.38;
+        font-size: 15px;
+      }
+      .input-group__details {
+        &:before {
+          background-color: #black;
+          opacity: 0.12;
         }
-        .notification-link{
-          display: block;
-          color: #1754be !important;
-          text-transform: uppercase;
-          font-size: 13px;
-          font-weight: 600;
-          float: left;
-          text-decoration: none;
-          font-family: 'Open Sans', sans-serif;
-          margin-top: 10px;
-        }
-        .close-notification{
-          color:$grey--text;
-          top: 9px;
-          position: absolute;
-          right: 9px;
-          box-shadow: none !important;
-          background: $white;
-          .icon{
-            font-size: 19px;
-            font-weight: 600;
+      }
+      .input-group--error {
+        .input-group__details {
+          &:before {
+            background-color: $blue-color;
           }
         }
-        .login-background {
-            position: absolute;
-            min-height: 50%;
-            width: 100%;
-            background-image: url(../assets/img/login-background.jpg);
-            background-size: cover;
-            z-index: 0;
-            top: 0;
-            left: 0;
-            z-index: 1;
-        }
-
-        .login-logo {
-          margin: auto;
-          display: block;
-          width:111px;
-        }
-        .login-main {
-            min-width: 410px;
-            z-index: 99;
-            flex:1;
-        }
-        .form-content {
-            padding: 25px;
-            label{
-                color:$black;
-                opacity:0.38;
-                font-size: 15px;
-            }
-            .input-group__details{
-                &:before{
-                    background-color: #black;
-                    opacity: 0.12;
-                }
-            }
-            .input-group--error{
-                .input-group__details{
-                    &:before{
-                        background-color: $blue-color;
-                    }
-                }
-            }
-        }
-        .submit-button-content{
-            padding-left: 15px;
-            padding-right: 15px;
-        }
-        .alert.error-message{
-            border: none !important;
-            padding: 0 20px 0 0;
-            justify-content: center;
-            div{
-                flex: inherit;
-            }
-        }
-        .notification-message{
-            color: $blue--text;
-            font-size: 14px;
-            font-family: 'Open Sans', sans-serif;
-            display: block;
-            float: left;
-            width: 100%;
-
-        }
-        .notification-content .snack__content{
-          max-width: 415px;
-          padding: 34px 29px 27px 29px;
-        }
-        .login-list{
-            bottom: 20px;
-            position: absolute;
-            left: 50%;
-            margin-top:0;
-            -webkit-transform: translateX(-50%);
-            transform: translateX(-50%);
-            overflow: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-            overflow: hidden;
-            word-wrap: break-word;
-            width: auto;
-            background: $bg-page;
-            .list__tile{
-                height: 19px;
-            }
-            .list__tile--link{
-                &:hover{
-                    background: transparent;
-                }
-            }
-            .login-navigation-bottom{
-                font-size:12px;
-                color:#8898aa;
-                cursor: pointer;
-                display: inline-block;
-                float:left;
-            }
-        }
-        .primary{
-            background-color:$blue-color;
-            height: 44px;
-        }
+      }
     }
+    .submit-button-content {
+      padding-left: 15px;
+      padding-right: 15px;
+    }
+    .alert.error-message {
+      border: none !important;
+      padding: 0 20px 0 0;
+      justify-content: center;
+      div {
+        flex: inherit;
+      }
+    }
+    .notification-message {
+      color: $blue--text;
+      font-size: 14px;
+      font-family: 'Open Sans', sans-serif;
+      display: block;
+      float: left;
+      width: 100%;
 
+    }
+    .notification-content .snack__content {
+      max-width: 415px;
+      padding: 34px 29px 27px 29px;
+    }
+    .login-list {
+      background: $bg-page;
+      .list__tile {
+        height: 19px;
+      }
+      .list__tile--link {
+        &:hover {
+          background: transparent;
+        }
+      }
+      .login-navigation-bottom {
+        font-size: 12px;
+        color: #8898aa;
+        cursor: pointer;
+        display: inline-block;
+        float: left;
+      }
+    }
+    .primary {
+      background-color: $blue-color;
+      height: 44px;
+    }
+  }
 </style>
