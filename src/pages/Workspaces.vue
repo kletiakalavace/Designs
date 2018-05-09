4496c5d34fda4f4ca4e26a11fd2964cc79948bb5
kletiakalavace <kletia@manoolia.com>
2018-05-09 15:13:07 +0200

    made width fix for block on workspace

--- src/pages/Workspaces.vue
+++ src/pages/Workspaces.vue
@@ -200,6 +200,7 @@
         transition-duration: 0.2s;
         border-radius: 2px;
         min-width: 244px;
+        width:244px;
         box-shadow: 0 2px 1px -3px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 1px 0 rgba(0,0,0,.12);
         -webkit-box-shadow:0 2px 1px -3px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 1px 0 rgba(0,0,0,.12);
         &:hover{
