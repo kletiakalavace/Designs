6fd3338518ce890fcf0302f7ae7adfa71a98f441
kletiakalavace <kletia@manoolia.com>
2018-05-24 13:10:52 +0200

    suppport page

--- src/pages/Support.vue
+++ src/pages/Support.vue
@@ -14,6 +14,31 @@
                 </v-breadcrumbs>
             </div>
         </template>
+        <v-flex xs12 sm6 lg8 offset-sm2 class="mx-auto content-support text-md-center">
+            <span class="thumb-workspace mx-auto d-block bg-light-green">S</span>
+            <h2 class="title blue--text mt-5">Support & Request</h2>
+            <p class="grey--text pt-1 mb-5">Get support/help and request services</p>
+        </v-flex>
+        <v-flex xs12 sm6 lg8 offset-sm2 class="mx-auto support-search">
+            <v-layout row bg-search mx-auto>
+                <v-flex lg12>
+                    <v-select
+                            label="Looking for software? Try typing 'adobe' or 'visio'..."
+                            :items="searches"
+                            item-text="name"
+                            item-value="name"
+                            v-model="e11"
+                            chips
+                            max-height="auto"
+                            autocomplete
+                            class="search-primary"
+                            content-class="select-searches"
+                            prepend-icon="search"
+                     >
+                        </v-select>
+                    </v-flex>
+                </v-layout>
+            </v-flex>
     </o-default-layout>
 </template>
 
@@ -65,5 +90,43 @@
         background-color: $bg-page;
         position:relative;
         z-index:1;
+    }
+    .thumb-workspace {
+        height: 70px;
+        width: 71px;
+        font-size: 32px;
+        color: #fff;
+        border-radius: 50% !important;
+        line-height: 72px;
+        text-align:center;
+    }
+    .content-support {
+        margin-top: 40px;
+        max-width: 791px !important;
+        h2.title.mt-5 {
+            font-size: 22px !important;
+            margin-top: 25px !important;
+            font-weight: 600;
+        }
+        p {
+            font-size: 15px;
+            font-weight: 400;
+        }
+        .blue--text{
+            color:$blue--text;
+        }
+    }
+    .support-search{
+        .bg-search{
+            background: $white;
+            border-radius: 2px;
+            max-width:811px;
+            height:48px;
+            label{
+                font-size: 15px;
+                color:#8898aa;
+            }
+
+        }
     }
 </style>
